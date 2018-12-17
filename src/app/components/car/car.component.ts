0208e3b033f9260265dd904f4bba772263db2158
Artem <1111255555@mail.ru>
2018-12-17 03:21:10 +0300

    v0.02

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
+  model:string;
+  speed:number;
+  name:string;
+  colors:Colors;
+  options:string[];
+
   constructor() { }
 
   ngOnInit() {
-  }
+    this.name = 'Audi';
+    this.speed = 300;
+    this.model = 'RS8';
+    this.colors = {
+      car: 'Белый',
+      salon: 'Черный',
+      wheels: 'Серибристый'
+    };
+    this.options = ["ABS","Автопилот","Авто паркинг"];
+    }
 
+    bmwSelect(){
+      this.name = 'BMW';
+      this.speed = 320;
+      this.model = 'M5';
+      this.colors = {
+        car: 'Синий',
+        salon: 'Белый',
+        wheels: 'Матовые'
+      };
+      this.options = ["ABS","Авто паркинг"];
+    }
+
+}
+
+interface Colors {
+  car:string,
+  salon:string,
+  wheels:string
 }
