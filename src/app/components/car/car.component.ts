b52c9e74468c537e4a4b791c2efde527dca5966e
Artem <1111255555@mail.ru>
2018-12-18 03:42:19 +0300

    v0.02

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -16,27 +16,59 @@ export class CarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.name = 'Audi';
-    this.speed = 300;
-    this.model = 'RS8';
+    this.name = 'Mersedes';
+    this.speed = 350;
+    this.model = 'майбах';
     this.colors = {
-      car: 'Белый',
-      salon: 'Черный',
-      wheels: 'Серибристый'
+      car: 'Золотой',
+      salon: 'Кожа',
+      wheels: 'Черный'
     };
-    this.options = ["ABS","Автопилот","Авто паркинг"];
+    this.options = ["ABS","Автопилот","Авто паркинг","Массажное кресло"];
+
     }
 
-    bmwSelect(){
-      this.name = 'BMW';
-      this.speed = 320;
-      this.model = 'M5';
-      this.colors = {
-        car: 'Синий',
-        salon: 'Белый',
-        wheels: 'Матовые'
-      };
-      this.options = ["ABS","Авто паркинг"];
+    addOpt(option){
+      this.options.unshift(option);
+      return false;
+    }
+    delOpt(option){
+
+    }
+
+    carSelect(carName){
+      if(carName == 'bmw'){
+          this.name = 'BMW';
+          this.speed = 320;
+          this.model = 'M5';
+          this.colors = {
+            car: 'Синий',
+            salon: 'Белый',
+            wheels: 'Матовые'
+          };
+          this.options = ["ABS","Авто паркинг"];
+      }else if(carName == 'audi'){
+        this.name = 'Audi';
+        this.speed = 300;
+        this.model = 'RS8';
+        this.colors = {
+          car: 'Белый',
+          salon: 'Черный',
+          wheels: 'Серибристый'
+        };
+        this.options = ["ABS"];
+      }else if(carName == 'mers'){
+        this.name = 'Mersedes';
+        this.speed = 350;
+        this.model = 'майбах';
+        this.colors = {
+          car: 'Золотой',
+          salon: 'Кожа',
+          wheels: 'Черный'
+        };
+        this.options = ["ABS","Автопилот","Авто паркинг","Массажное кресло"];
+      }
+
     }
 
 }
